c65367763815b8c780bd83be046f262e6a26e19f
Robert Stern <rstern@gmail.com>
2017-06-17 18:07:45 -0400

    Add rpio data

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 var http = require('http');
 var eddystoneBeacon = require('eddystone-beacon');
+var bleno = require('bleno');
 
-var url = 'https://smartcooler.io';
+var characteristic = new bleno.Characteristic({
+	uuid: '98207dcc-53a2-11e7-9608-b827ebacd514',
+	properties: ['read'],
+	value: new Buffer('example')
+});
+
+var service = new bleno.PrimaryService({
+	uuid: '98207dcc-53a2-11e7-9608-b827ebacd514',
+	characteristics: [
+		characteristic
+	]
+});
+
+bleno.once('advertistingStart', function(err) {
+	if (err) {
+		throw err;
+	}
+
+	console.log('on -> advertisingStart');
+	bleno.setServices([
+		service
+	]);
+});
+
+//var url = 'https://goo.gl/G88oMo';
+//var url = 'https://smartcooler.io';
+var url = 'https://goo.gl/UWeSPT';
 
 var options = {
-	name: 'Raspberry Pi',
+	name: 'Smart Cooler',
 	txPwerLevel: -22,
 	tlmCount: 2,
 	tlmPeriod: 10
