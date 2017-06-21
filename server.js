37a6dad8243046ec8513478a687468196a4f5167
Robert Stern <rstern@gmail.com>
2017-06-21 01:18:27 -0400

    Add actual temperature notifications

--- server.js
+++ server.js
@@ -1,29 +1,15 @@
 var eddystoneBeacon = require('eddystone-beacon');
 var express = require('express');
-var bleno = require('bleno');
+var bleno = require('eddystone-beacon/node_modules/bleno');
 var app = express();
 
-// BLE Beacon
-
-//var url = 'https://goo.gl/G88oMo';
-//var url = 'https://goo.gl/UWeSPT';
-var url = 'https://smartcooler.io';
-
-var options = {
-  name: 'Smart Cooler',
-  txPwerLevel: -22,
-  tlmCount: 2,
-  tlmPeriod: 10
-};
-
-eddystoneBeacon.advertiseUrl(url, options);
-
 // BLE Temperature
+const NO_SAMPLES = 10;
 class TemperatureCharacteristic extends bleno.Characteristic {
   constructor() {
     super({
       uuid: 'f94dcd49-148b-47e6-b102-f115223cd282',
-      properties: ['read', 'write', 'notify'],
+      properties: ['read', 'notify'],
       value: null
     });
 
@@ -51,7 +37,6 @@ class TemperatureCharacteristic extends bleno.Characteristic {
   }
 
   valueChange(value) {
-    const NO_SAMPLES = 100;
     this._total += value;
     this._samples++;
 
@@ -59,31 +44,56 @@ class TemperatureCharacteristic extends bleno.Characteristic {
       return;
     }
 
-    var newValue = Math.round(this._total / NO_SAMPLES);
+    var newValue = (this._total / NO_SAMPLES).toFixed(2);
     this._total = 0;
     this._samples = 0;
 
-    if (this._lastValue && Math.abs(this._lastValue - newValue) < 1) {
+    if (this._lastValue && Math.abs(this._lastValue - newValue) < 0.1) {
       return;
     }
 
     this._lastValue = newValue;
 
-    console.log(newValue);
     var data = new Buffer(8);
 
     data.writeDoubleLE(newValue, 0);
 
     if (this._onChange) {
+      console.log('Notifying ' + newValue);
       this._onChange(data);
     }
   }
 }
 
+// BLE Beacon
+
+//var url = 'https://goo.gl/G88oMo';
+//var url = 'https://goo.gl/UWeSPT';
+var url = 'https://smartcooler.io';
+
+var options = {
+  name: 'Smart Cooler',
+};
+
+bleno.on('stateChange', function(state) {
+  console.log('state change ' + state);
+  if (state === 'poweredOn') {
+    bleno.startAdvertising('Smart Cooler', ['e24e9ac7-a763-4f1d-a582-010447333ffc']);
+    eddystoneBeacon.advertiseUrl(url, options);
+  } else {
+    bleno.stopAdvertising();
+  }
+});
+
+var temperatureCharacteristic = new TemperatureCharacteristic;
 bleno.on('advertisingStart', function(error) {
+  if (error) {
+    console.log(error);
+  }
+
   bleno.setServices([new bleno.PrimaryService({
     uuid: 'e24e9ac7-a763-4f1d-a582-010447333ffc',
-    characteristics: [TemperatureCharacteristic]
+    characteristics: [temperatureCharacteristic]
   })]);
 });
 
@@ -100,13 +110,17 @@ var port = new SerialPort('/dev/ttyACM0', {
 });
 
 var jsonData = {};
-port.on('open', function() {
+port.on('open', () => {
   console.log('open serial communication');
-  port.on('data', function(data) {
+  port.on('data', (data) => {
     //console.log(data.toString());
     try {
       jsonData = JSON.parse(data);
     } catch (err) {
+    }
+
+    if (jsonData.temperature) {
+      temperatureCharacteristic.valueChange(jsonData.temperature);
     }
   });
 });
